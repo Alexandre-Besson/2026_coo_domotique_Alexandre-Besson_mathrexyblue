d85efd92b8f7cb9ab41440d75cfcebdd2f0e38cf
Mathieu Allibert-Millot <e24719u@etu.univ-lorraine.fr>
2026-03-31 11:23:45 +0200

    ajout de télécommande

--- DOMOTIQUE/src/Telecommande.java
+++ DOMOTIQUE/src/Telecommande.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Telecommande {
+
+    public Telecommande(){
+        throw new Error("TO DO");
+    }
+
+    public void ajouterLampe(Lampe l){
+        throw new Error("TO DO");
+    }
+
+    public void activerLampe(int indiceLampe){
+        throw new Error("TO DO");
+    }
+
+    public void desactiverLampe(int indiceLampe){
+        throw new Error("TO DO");
+    }
+
+    public void activerTout(){
+        throw new Error("TO DO");
+    }
+
+    public String toString(){
+        throw new Error("TO DO");
+    }
 }
