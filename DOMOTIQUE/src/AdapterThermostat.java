7f134c89906082d6706cc7c90881b29c5855f6fd
Mathieu Allibert-Millot <e24719u@etu.univ-lorraine.fr>
2026-04-07 15:49:15 +0200

    ajout de thermostat et de son adaptateur

---
+++ DOMOTIQUE/src/AdapterThermostat.java
@@ -0,0 +1,24 @@
+import thermos.Thermostat;
+
+public class AdapterThermostat implements Appareil{
+    private Thermostat thermostat;
+
+    public AdapterThermostat(Thermostat thermostat) {
+        this.thermostat=thermostat;
+    }
+
+    @Override
+    public void eteindre() {
+        this.thermostat.baisserTemperature();
+    }
+
+    @Override
+    public void allumer() {
+        this.thermostat.monterTemperature();
+    }
+
+    @Override
+    public String toString(){
+        return this.thermostat.toString();
+    }
+}
