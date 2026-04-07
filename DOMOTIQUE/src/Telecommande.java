9b1d01f007ff1a264ffa8dce8a9f3ceef9d86fb2
Mathieu Allibert-Millot <e24719u@etu.univ-lorraine.fr>
2026-04-07 09:27:11 +0200

    modification de telecommande pour prendre en compte l'interface appareil

--- DOMOTIQUE/src/Telecommande.java
+++ DOMOTIQUE/src/Telecommande.java
@@ -4,96 +4,102 @@ import java.util.*;
 
 /**
  * CLASSE A PRODUIRE PAR LES ETUDIANTS
- * 
- * La classe Telecommande permet de manipuler une liste de Lampe. Elle sera
- * amenee a commander une liste d'appareil plus generiques par la suite.
- * 
+ *
+ * La classe Telecommande permet de manipuler une liste d'Appareil.
+ *
  * on peut rajouter des get avec un entier en parametre pour permettre les tests
  * mais pas de set.
- * 
+ *
  */
 public class Telecommande {
 
 	/**
-	 * la liste des lampes a controler on utilise l'interface pour masquer
+	 * la liste des appareils a controler on utilise l'interface pour masquer
 	 * l'implementation a ce niveau
 	 */
-	private List<Lampe> lampes;
+	private List<Appareil> appareils;
 
 	/**
-	 * construit une telecommande sans lampe
+	 * construit une telecommande sans appareil
 	 */
 	public Telecommande() {
-		this.lampes = new ArrayList<Lampe>();
+		this.appareils = new ArrayList<Appareil>();
 	}
 
 	/**
-	 * on ajoute une lampe a la liste des lampes controlees
-	 * 
-	 * @param nouvelleLampe
-	 *            nouvelle lampe a controler
+	 * on ajoute un appareil a la liste des appareils controlees
+	 *
+	 * @param nouvelAppareil
+	 *            nouvel appareil a controler
 	 */
-	public void ajouterLampe(Lampe nouvelleLampe) {
-		this.lampes.add(nouvelleLampe);
+	public void ajouterAppareil(Appareil nouvelAppareil) {
+		this.appareils.add(nouvelAppareil);
 	}
 
 	/**
-	 * permet d'activer une lampe a partir de la telecommande
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a activer dans la telecommande
+	 * permet d'activer un appareil a partir de la telecommande
+	 *
+	 * @param indiceAppareil
+	 *            indice de l'appareil a activer dans la telecommande
 	 */
-	public void activerLampe(int indiceLampe) {
-		// TODO les etudiants devraient verifier qu'on ne deborde pas du tableau
-		// dans leurs tests
-		if (indiceLampe < this.lampes.size())
-			this.lampes.get(indiceLampe).allumer();
+	public void activerAppareil(int indiceAppareil) {
+
+		if (indiceAppareil < this.appareils.size())
+			this.appareils.get(indiceAppareil).allumer();
 	}
 
 	/**
-	 * permet de desactiver une lampe en utilisant la telecommande
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a eteindre
+	 * permet de desactiver un appareil en utilisant la telecommande
+	 *
+	 * @param indiceAppareil
+	 *            indice de l'appareil a eteindre
 	 */
-	public void desactiverLampe(int indiceLampe) {
-		if (indiceLampe < this.lampes.size())
-			this.lampes.get(indiceLampe).eteindre();
+	public void desactiverAppareil(int indiceAppareil) {
+		if (indiceAppareil < this.appareils.size())
+			this.appareils.get(indiceAppareil).eteindre();
 	}
 
 	/**
-	 * permet d'activer l'ensemble des lampes
+	 * permet d'activer l'ensemble des appareils
 	 */
 	public void activerTout() {
-		for (int i = 0; i < this.lampes.size(); i++)
+		for (int i = 0; i < this.appareils.size(); i++)
 			// reutiliser du code existant
-			this.activerLampe(i);
+			this.activerAppareil(i);
 	}
 
 	/**
-	 * descriptif de la telecommande retourne une ligne par lampe suivi du
-	 * status de la lampe.
+	 * descriptif de la telecommande retourne une ligne par appareil suivi du
+	 * status de l'appareil.
 	 */
 	public String toString() {
 		String descriptif = "";
-		for (int i = 0; i < this.lampes.size(); i++) {
-			descriptif += i + "-" + this.lampes.get(i) + "\n";
+		for (int i = 0; i < this.appareils.size(); i++) {
+			descriptif += i + "-" + this.appareils.get(i) + "\n";
 		}
 		return (descriptif);
 	}
 
 	/**
-	 * get qui retourne une lampe utile pour les tests
-	 * 
-	 * @param indiceLampe
-	 *            indice de la lampe a tester
-	 * @return la lampe a l'indice indiceLampe
+	 * get qui retourne un appareil utile pour les tests
+	 *
+	 * @param indiceAppareil
+	 *            indice de l'appareil a tester
+	 * @return l'appareil a l'indice indiceAppareil
 	 */
-	public Lampe getLampe(int indiceLampe) {
-		if (indiceLampe < this.lampes.size())
-			return (this.lampes.get(indiceLampe));
+	public Appareil getAppareil(int indiceAppareil) {
+		if (indiceAppareil < this.appareils.size())
+			return (this.appareils.get(indiceAppareil));
 		else
 			return(null);
+	}
+
+	/**
+	 * get qui retourne la liste d'appareils
+	 * @return la liste d'appareils
+	 */
+	public List<Appareil> getAppareils(){
+		return this.appareils;
 	}
 
 }
